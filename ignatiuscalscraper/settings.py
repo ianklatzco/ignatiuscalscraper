a3c9dd5a24c55df74121e6a00aaf5930c2eff7cd
ianklatzco <iklatzco@gmail.com>
2014-03-06 20:16:13 -0600

    first commit of scrapy configs, half-worked out

---
+++ ignatiuscalscraper/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for ignatiuscalscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ignatiuscalscraper'
+
+SPIDER_MODULES = ['ignatiuscalscraper.spiders']
+NEWSPIDER_MODULE = 'ignatiuscalscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ignatiuscalscraper (+http://www.yourdomain.com)'
---
+++ ignatiuscalscraper/spiders/test.py
@@ -0,0 +1,25 @@
+from scrapy.spider import Spider
+from scrapy.selector import HtmlXPathSelector
+# from craigslist_sample.items import CraigslistSampleItem
+
+class IgnCalSpider(Spider):
+	name = "igcal"
+	allowed_domains = ["ignatius.org"]
+	start_urls = ["http://www.ignatius.org/calendar.aspx"]
+
+
+	def parse(self, response):
+		hxs = HtmlXPathSelector(response)
+		titles = hxs.select("//span[@class='BodyText']")
+								# I think this is right.
+		items = []
+		for titles in titles:
+			item = CraigslistSampleItem()
+			item ["title"] = titles.select("a/td()").extract()
+			# I'm not sure what to select here^^^
+			items.append(item)
+		return items
+
+#xpath of the a day/p day td elements
+# //*[@id="ctl00_cphMainContent_dgCalendar_ctl04_DataList1"]/tbody/tr[1]/td
+# //*[@id="ctl00_cphMainContent_dgCalendar_ctl05_DataList1"]/tbody/tr[1]/td
